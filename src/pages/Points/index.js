f765e32372acf7434d874f6f63a3f47a7cf51df6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-06 12:42:46 -0300

    Poinst page map and item list

--- src/pages/Points/index.js
+++ src/pages/Points/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import Constants from 'expo-constants'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  Image
+} from 'react-native'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
+import MapView, { Marker } from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
+
+import marketIcon from 'app/assets/market.png'
 
 const Points = () => {
   const navigation = useNavigation()
@@ -12,11 +23,88 @@ const Points = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleNavigateBack}>
-        <Icon name="arrow-left" size={20} color="#34cb79" />
-      </TouchableOpacity>
-    </View>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -23.5954058,
+              longitude: -46.8543619,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                  latitude: -23.5954058,
+                  longitude: -46.8543619
+                }}
+            >
+              <View>
+
+                <Image
+                  style={styles.mapMarkerImage}
+                  source={marketIcon}
+                />
+              </View>
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+          horizontal
+        >
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item}>
+            <SvgUri width={42} height={42} uri="https://282ad7469b30.ngrok.io/uploads/oleo.svg" />
+
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
   )
 }
 
@@ -27,5 +115,93 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 32,
     paddingTop: 20 + Constants.statusBarHeight,
-  }
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    height: 60,
+    resizeMode: 'contain'
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
 });
